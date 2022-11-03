71e2167c186e188678ce56ac58c1c8c71d11bfdf
LeeSuHa98 <lapi12@naver.com>
2022-11-04 03:32:00 +0900

    :sparkles: new ChatBot Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, styled } from "@mui/material";
+import ChatBot from "./component/chatbot/ChatBot";
 
+const BodyCss = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Container>
+            <BodyCss>
+                <ChatBot/>
+            </BodyCss>
+        </Container>
+    </>
   );
 }
 
